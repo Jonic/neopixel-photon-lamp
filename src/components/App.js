b2c6ad9389ea786914eab7bf88a03f8ab97924a2
Jonic Linley <jonic@raspberrypi.org>
2018-01-19 17:36:19 +0000

    Enabled cookie for session storage

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,13 @@
-import { Container, Slice, TypeDisplayLarge } from "raspberry-pi-bits"
-import Particle from "particle-api-js"
-import React, { Component } from "react"
+import { Container, Slice, TypeDisplayLarge } from 'raspberry-pi-bits'
+import { withCookies, Cookies } from 'react-cookie'
+import Particle from 'particle-api-js'
+import { instanceOf } from 'prop-types'
+import React, { Component } from 'react'
 
-import Authenticate from "./Authenticate/Authenticate"
-import DeviceManager from "./DeviceManager/DeviceManager"
+import Authenticate from './Authenticate/Authenticate'
+import DeviceManager from './DeviceManager/DeviceManager'
 
-import "./App.css"
+import './App.css'
 
 const particle = new Particle()
 
@@ -13,10 +15,12 @@ class App extends Component {
   constructor(props) {
     super(props)
 
+    const { cookies } = this.props
+
     this.state = {
-      authenticated: false,
-      deviceId: process.env.REACT_APP_PARTICLE_DEVICE_ID,
-      token: null,
+      authenticated: cookies.get('authenticated') || false,
+      deviceId:      process.env.REACT_APP_PARTICLE_DEVICE_ID,
+      token:         cookies.get('token') || null,
     }
   }
 
@@ -24,13 +28,23 @@ class App extends Component {
     this.setState(
       {
         authenticated: true,
-        token: token,
+        token:         token,
       },
-      this.getDeviceInfo,
+      () => {
+        this.storeAuthenticationDetails()
+        this.getDeviceInfo()
+      }
     )
   }
 
   getDeviceInfo = () => {}
+
+  storeAuthenticationDetails = () => {
+    const { cookies } = this.props
+
+    cookies.set('authenticated', this.state.authenticated, { path: '/' })
+    cookies.set('token', this.state.token, { path: '/' })
+  }
 
   render() {
     return (
@@ -38,15 +52,18 @@ class App extends Component {
         <Container>
           <TypeDisplayLarge element="h1">IOT Lamp</TypeDisplayLarge>
 
-          {this.state.authenticated ? (
-            <DeviceManager deviceId={this.state.deviceId} />
-          ) : (
-            <Authenticate authenticationCallback={this.authenticationCallback} particle={particle} />
-          )}
+          {this.state.authenticated
+            ? <DeviceManager deviceId={this.state.deviceId} />
+            : <Authenticate authenticationCallback={this.authenticationCallback} particle={particle} />
+          }
         </Container>
       </Slice>
     )
   }
 }
 
-export default App
+App.propTypes = {
+  cookies: instanceOf(Cookies).isRequired,
+}
+
+export default withCookies(App)
